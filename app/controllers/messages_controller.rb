4895fd7aaec246c2c0b89ccb146415da78e8c8a9
DGP115 <david.g.peloso@gmail.com>
2023-01-01 22:50:31 -0500

    Add Attachemnts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,12 +4,13 @@
 class MessagesController < ApplicationController
   def create
     @message = current_user.messages.create(body: whitelist_msg_params[:body],
-                                            room_id: params[:room_id])
+                                            room_id: params[:room_id],
+                                            attachments: whitelist_msg_params[:attachments])
   end
 
   private
 
   def whitelist_msg_params
-    params.require(:message).permit(:body)
+    params.require(:message).permit(:body, attachments: [])
   end
 end
